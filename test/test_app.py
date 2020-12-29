4ad14ed28841a12ae7831380fccf46946b0e588e
supermasita <supermasita@supermasita.com>
2020-12-29 13:03:55 -0300

    Refactor

--- test/test_app.py
+++ test/test_app.py
@@ -2,11 +2,10 @@ import unittest
 import requests
 
 
-class TestStringMethods(unittest.TestCase):
+class TestGeoipFlask(unittest.TestCase):
 
     def test_app_json_ipv4(self):
-        geoip_response = requests.get(
-            "http://127.0.0.1:8888/api/v1.0/ip/200.42.143.3")
+        geoip_response = requests.get("http://127.0.0.1:8888/api/v1.0/ip/200.42.143.3")
         geoip_response = geoip_response.json()
         self.assertEqual(geoip_response['country']['iso_code'], "AR")
 
@@ -15,8 +14,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertRegex(geoip_response.text, "IP: <b>200.42.143.3</b>")
 
     def test_app_json_ipv6(self):
-        geoip_response = requests.get(
-            "http://127.0.0.1:8888/api/v1.0/ip/2001:4860:4860::8888")
+        geoip_response = requests.get("http://127.0.0.1:8888/api/v1.0/ip/2001:4860:4860::8888")
         geoip_response = geoip_response.json()
         self.assertEqual(geoip_response['country']['iso_code'], "US")
 
@@ -29,6 +27,11 @@ class TestStringMethods(unittest.TestCase):
     def test_app_ip_null(self):
         geoip_response = requests.get("http://127.0.0.1:8888/23.155.53.186")
         self.assertRegex(geoip_response.text, "IP: <b>23.155.53.186</b>")
+
+    def test_app_json_with_proxy_header(self):
+        geoip_response = requests.get("http://127.0.0.1:8888/api/v1.0/ip/", headers={"X-Real-IP": "200.42.143.3"})
+        geoip_response = geoip_response.json()
+        self.assertEqual(geoip_response['country']['iso_code'], "AR")
 
 
 if __name__ == '__main__':
